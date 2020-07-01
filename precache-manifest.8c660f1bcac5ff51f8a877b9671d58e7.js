73cc8cb4234655f25fd9ca1837863dbd43c3084b
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-07-01 21:35:08 +0200

    Updates

---
+++ precache-manifest.8c660f1bcac5ff51f8a877b9671d58e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957ed5de84227f228b32cff3ab329c58",
+    "url": "/contact-react/index.html"
+  },
+  {
+    "revision": "7d3833c1799b9221be83",
+    "url": "/contact-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6519aa9cf0eb782122c1",
+    "url": "/contact-react/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "7d3833c1799b9221be83",
+    "url": "/contact-react/static/js/2.9ce674b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-react/static/js/2.9ce674b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6519aa9cf0eb782122c1",
+    "url": "/contact-react/static/js/main.4dc48a01.chunk.js"
+  },
+  {
+    "revision": "f0016085cdd5213e919a",
+    "url": "/contact-react/static/js/runtime-main.020c4f82.js"
+  }
+]);
